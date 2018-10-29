3eae33666e4b5e925a426c6b1303fd39e8098fdd
Iurii Drozdov <ydrozdov@gmail.com>
2018-10-29 13:43:59 +0100

    moved Task1 solution to separate class

--- src/Task1.java
+++ src/Task1.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Task1 {
+    public int solution(int[] A) {
+        int length = 0;
+
+        if (A.length < 1) {
+            return length;
+        }
+
+        int current = A[0];
+
+        while (current != -1) {
+            current = A[current];
+            length++;
+        }
+
+        return ++length;
+    }
 }
